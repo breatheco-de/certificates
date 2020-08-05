f037e7e9910bff71eb69cac3089d8abecc27f065
Omonroy36 <monroyorlando.36@gmail.com>
2020-08-05 15:34:33 +0000

    translations added}

--- src/certificates/diploma/es.js
+++ src/certificates/diploma/es.js
@@ -4,7 +4,7 @@ let es = {
     "Development":"Desarrollo",
     "Recognizes that": "Reconoce que",
     "Has Successfully Completed":"Ha completado con éxito",
-    "The Full Stack Development Program":"El programa de desarrollo Full Stack",
+    "The Full Stack Development Program":"El programa Full Stack Software Development",
     "Hours": "Horas",
     "Lead Instructor":"Instructor Principal",
     "Co-founder":"Co-fundador",
