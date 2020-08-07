020a844db3c6c72acfbf8d4c185d782c80b80941
Omonroy36 <monroyorlando.36@gmail.com>
2020-08-07 19:23:51 +0000

    language switcher added

---
+++ src/certificates/components/LanguageSwitcher/language-switcher.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
+const Icon = ({ lang, className }) =>
+    // <div className={"icon btn btn-sm "+className} style={{ backgroundImage: `url(${url}` }}>{' '}</div>;
+    <div className={"icon " + className} style={{ backgroundImage: `url(https://www.countryflags.io/${lang}/flat/64.png` }}>{' '}</div>;
+Icon.propTypes = {
+    url: PropTypes.string,
+    className: PropTypes.string,
+};
+Icon.defaultProps = {
+    className: '',
+};
+
+const LanguageSwitcher = ({ current, translations, onClick }) => {
+    return (<div className="language-switcher">
+        <ul>
+            {
+                translations.filter(lang => lang !== current).map(lang => {
+                    return (<li><span onClick={() => onClick(lang)}><Icon lang={lang == "en" ? "us" : lang} /></span></li>);
+                })
+            }
+        </ul>
+        <span><Icon lang={current == 'en' ? 'us' : current} /></span>
+    </div>);
+};
+
+export default LanguageSwitcher;
